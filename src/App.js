3caaa1692b815bd0d27ecc169e6fffe7e3f1a82d
oredola <oredolaolamide@gmail.com>
2019-11-11 15:14:53 +0100

    Added exact prop for root component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Home, About, Contact, Navigation } from './components';
 const App = () => (
   <div>
     <Navigation />
-    <Route path="/" component={Home} />
+    <Route exact path="/" component={Home} />
     <Route path="/about" component={About} />
     <Route path="/contact" component={Contact} />
   </div>
